04030885f99d12fe22b0a884479c213186a52c49
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-07-13 15:08:46 +0530

    [Nithin] Add : updation of TwoPlayers

--- SnakeAndLader/SnakeAndLader/Program.cs
+++ SnakeAndLader/SnakeAndLader/Program.cs
@@ -1,13 +1,34 @@
 ﻿using System;
+
 namespace SnakeAndLader
 {
-
-
-    internal class Program
+    class Program
     {
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
-            
+            Snake player1 = new Snake();
+            Snake player2 = new Snake();
+
+            string player1position = "";
+            string player2position = "";
+
+            while (!player1.GetWon() || !player2.GetWon())
+            {
+
+                player1position += player1.PlayGame() + " [] ";
+                player2position += player2.PlayGame() + " [] ";
+
+            }
+            Console.WriteLine("position of player1 \n" + player1position);
+            Console.WriteLine("position of player2 \n" + player2position);
+            Console.WriteLine("the total number of dice rolled " + player1.GetCount());
+            Console.WriteLine("the total number of dice rolled " + player2.GetCount());
+
+            //Console.WriteLine( "last postion p1 "+player1.GetPosition());
+            //Console.WriteLine( "last postion p2 "+player2.GetPosition());
+            if (player1.GetWon()) Console.WriteLine("player 1 won");
+            else if (player2.GetWon()) Console.WriteLine("player 2 won"); 
+
 
         }
     }
