bd9f193714d149b9a6d34049da04ec31fe22a82a
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-07-13 11:48:01 +0530

    [Nithin] Add: updation of position

---
+++ SnakeAndLader/SnakeAndLader/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SnakeAndLader
+{
+
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ SnakeAndLader/SnakeAndLader/Snake.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLader
+{
+    internal class Snake
+    {
+        const int NO_PLAY = 0, LADDER = 1, SNAKE = 2, WINNING_POSITION = 100, STARTING_POSITION = 0;
+        //uc1
+        int position = 0, count = 0;
+    }
+}
+
+       
