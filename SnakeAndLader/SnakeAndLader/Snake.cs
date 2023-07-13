d4670d45b3deb6e1925cb4c989a9845c75cc9a85
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-07-13 12:29:50 +0530

    [Nithin] Add :updation of DiePosition

--- SnakeAndLader/SnakeAndLader/Snake.cs
+++ SnakeAndLader/SnakeAndLader/Snake.cs
@@ -11,8 +11,16 @@ namespace SnakeAndLader
 
         //uc1
         int position = 0;
- 
-        
+        Random random = new Random();
+
+        //uc2
+        public void DieRoll()
+        {
+            int diePosition = random.Next(1, 7);
+            Console.WriteLine("Die POsition is" + diePosition);
+        }
+
+
     }
 }
 
